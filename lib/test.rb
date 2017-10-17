5a99b5121d37d62c476337a9072c023c8eb291c6
Stefano <stefano@seluxit.com>
2017-10-17 21:48:49 +0200

    Update test.rb

--- lib/test.rb
+++ lib/test.rb
@@ -3,7 +3,7 @@ OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 require_relative "Alphavantage"
 
-Alphavantage::AV.key = "HC7GKYN1TGKKUNFY"
+Alphavantage::AV.key = ""
 msft = Alphavantage::AV.new symbol: "MSFT"
 val = msft.intraday
 msft2 = Alphavantage::Timeseries.new type: "daily", symbol: "MSFT"
